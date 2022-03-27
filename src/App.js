0bca490719b71891db6ee7ac9cdf4f4e4f61594e
vishnugprasad <vishnuanugraha001@gmail.com>
2022-03-27 21:14:26 +0530

    container two

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setLogin(false);
   };
   return (
-    <div className={`dropMenu ${className}`}>  {/* what is the purpose */}
+    <div className={`dropMenu ${className}`}>  
       <DropMenu></DropMenu>
 
       
--- src/Seatbookin.js
+++ src/Seatbookin.js
@@ -5,14 +5,13 @@ import Calendar from "./Calendar";
 import { useState } from "react";
 
 const Seatbookin = () => {
-  const [date, setDate] = useState("")
-  var dcount = date.toString().split(" ")
+  const [date, setDate] = useState("");
+  var dcount = date.toString().split(" ");
 
   return (
     <div className="bookingContainer">
-      <Calendar date = {date}
-      setDate = {setDate}></Calendar>
-      
+      <Calendar date={date} setDate={setDate}></Calendar>
+
       <div className="BookingScreen">
         <div className="cubicle">
           {/* <button className="seat"></button> */}
@@ -143,27 +142,32 @@ const Seatbookin = () => {
 
         <div className="cubicle">
           <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
+            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
           </IconButton>
 
           <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
+            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
           </IconButton>
 
           <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
+            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
           </IconButton>
 
           <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
+            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
           </IconButton>
         </div>
       </div>
-      <div className = "dateSelection">{
 
-      date !== "" ?
-       `${dcount[0]} ${dcount[1]} ${dcount[2]} ${dcount[3]}` : null } 
-        {/* // when string and variable needed to be printed together "$" "``" is used */}
+      <div className=" containerTwo">
+        <div className="instructionBox">instruction</div>
+        <div className="dateSelection">
+          {date !== ""
+            ? `${dcount[0]} ${dcount[1]} ${dcount[2]} ${dcount[3]}`
+            : null}
+          {/* // when string and variable needed to be printed together "$" "``" is used */}
+        </div>
+        <button className="bookButton">Book Seat</button>
       </div>
     </div>
   );
