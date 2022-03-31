90987b88618924ce42305919b400704d4b5e009f
vishnugprasad <vishnuanugraha001@gmail.com>
2022-03-31 20:19:30 +0530

    update

--- src/Seatbookin.js
+++ src/Seatbookin.js
@@ -1,6 +1,4 @@
 import "./Seatbookin.css";
-import PersonIcon from "@mui/icons-material/Person";
-import IconButton from "@mui/material/IconButton";
 import Calendar from "./Calendar";
 import { useState } from "react";
 
@@ -14,148 +12,89 @@ const Seatbookin = () => {
 
       <div className="BookingScreen">
         <div className="cubicle">
-          {/* <button className="seat"></button> */}
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "yellow" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "yellow" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
         </div>
 
         <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "green" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+          
         </div>
 
         <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "green" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+        
+        </div>
+        <div className="cubicle">
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+        </div>
+        <div className="cubicle">
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+         
         </div>
 
         <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
         </div>
 
         <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
         </div>
 
         <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-        </div>
-
-        <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "pink" }}> </PersonIcon>
-          </IconButton>
-        </div>
-
-        <div className="cubicle">
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
-          </IconButton>
-
-          <IconButton aria-label="delete" className="seat">
-            <PersonIcon sx={{ color: "red" }}> </PersonIcon>
-          </IconButton>
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+          
+          <button className="seat"></button>
+         
         </div>
       </div>
 
