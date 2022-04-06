16defd2003c266568b3c1cd14f32125c574c36f2
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-06 15:47:02 +0530

    booking updated

--- src/Seatbookin.js
+++ src/Seatbookin.js
@@ -13,21 +13,32 @@ import { useEffect, useState } from "react";
 
 const Seatbookin = () => {
   const getClassName = (value)=> {
+    
     if(selectedSeat.includes(value)) {
     return "previousBookedSeat"
     }
      else if (value === currSelectedSeat) {
-       return "currSelectedSeat "
+       return "currSelectedSeat "   //class is returned
      }
 
      else 
-     return "seat"
+     return "seat" //class is returned
     
   }
+  const[isBooked, setIsBooked] = useState(false)  // to change the color to violet on selection
   const [currSelectedSeat, setCurrSelectedSeat] = useState(null)
   const [selectedSeat, setSelectedSeat] = useState([]);
   const [date, setDate] = useState("");
+  useEffect(()=> { 
+
+    setCurrSelectedSeat(null)
+    setIsBooked(false);
+  
+
+  }, [date])
+  
   var dcount;
+  // const [color, setColor] = useState(null)
   useEffect(() => {
     // console.log(date);
     const currDate =
@@ -56,7 +67,8 @@ const Seatbookin = () => {
              setCurrSelectedSeat(1)
               console.log(selectedSeat);
             }}
-            className={getClassName(1)}
+            className={getClassName(1)} 
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -64,6 +76,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(2)
             }}
             className={getClassName(2)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -71,6 +84,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(3)
             }}
             className={getClassName(3)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -78,6 +92,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(4)
             }}
             className={getClassName(4)}
+            disabled = {isBooked}
           ></button>
         </div>
 
@@ -87,6 +102,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(5)
             }}
             className={getClassName(5)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -94,12 +110,14 @@ const Seatbookin = () => {
               setCurrSelectedSeat(6)
             }}
             className={getClassName(6)}
+            disabled = {isBooked}
           ></button>
           <button
             onClick={() => {
               setCurrSelectedSeat(7)
             }}
             className={getClassName(7)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -107,6 +125,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(8)
             }}
             className={getClassName(8)}
+            disabled = {isBooked}
           ></button>
         </div>
 
@@ -116,6 +135,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(9)
             }}
             className={getClassName(9)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -123,6 +143,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(10)
             }}
             className={getClassName(10)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -130,6 +151,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(11)
             }}
             className={getClassName(11)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -137,6 +159,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(12)
             }}
             className={getClassName(12)}
+            disabled = {isBooked}
           ></button>
         </div>
         <div className="cubicle">
@@ -145,6 +168,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(13)
             }}
             className={getClassName(13)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -152,6 +176,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(14)
             }}
             className={getClassName(14)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -159,6 +184,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(15)
             }}
             className={getClassName(15)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -166,6 +192,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(16)
             }}
             className={getClassName(16)}
+            disabled = {isBooked}
           ></button>
         </div>
         <div className="cubicle">
@@ -174,6 +201,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(17)
             }}
             className={getClassName(17)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -181,6 +209,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(18)
             }}
             className={getClassName(18)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -188,6 +217,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(19)
             }}
             className={getClassName(19)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -195,6 +225,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(20)
             }}
             className={getClassName(20)}
+            disabled = {isBooked}
           ></button>
         </div>
 
@@ -204,6 +235,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(21)
             }}
             className={getClassName(21)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -211,6 +243,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(22)
             }}
             className={getClassName(22)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -218,6 +251,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(23)
             }}
             className={getClassName(23)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -225,6 +259,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(24)
             }}
             className={getClassName(24)}
+            disabled = {isBooked}
           ></button>
         </div>
 
@@ -234,6 +269,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(25)
             }}
             className={getClassName(25)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -241,6 +277,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(26)
             }}
             className={getClassName(26)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -248,6 +285,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(27)
             }}
             className={getClassName(27)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -255,6 +293,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(28)
             }}
             className={getClassName(28)}
+            disabled = {isBooked}
           ></button>
         </div>
 
@@ -264,6 +303,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(29)
             }}
             className={getClassName(29)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -271,6 +311,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(30)
             }}
             className={getClassName(30)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -278,6 +319,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(31)
             }}
             className={getClassName(31)}
+            disabled = {isBooked}
           ></button>
 
           <button
@@ -285,6 +327,7 @@ const Seatbookin = () => {
               setCurrSelectedSeat(32)
             }}
             className={getClassName(32)}
+            disabled = {isBooked}
           ></button>
         </div>
       </div>
@@ -311,12 +354,15 @@ const Seatbookin = () => {
 
             if (currDate) {
               const seats = [...selectedSeat, currSelectedSeat]
+              setSelectedSeat(seats)
               localStorage.setItem(currDate, JSON.stringify(seats));
+
+              setIsBooked(true)
             }
           }}
-          className="bookButton"
+          className={isBooked ?"bookedConfirm": "bookButton"}
         >
-          Book Seat
+          {isBooked ? "BOOKED" : "CONFIRM BOOKING"}
         </button>
       </div>
     </div>
